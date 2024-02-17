4ace16946d3b8a5889301f106d3b97dfc85e5505
Minn Banya <minnbanya@gmail.com>
2024-02-17 21:57:12 +0700

    website updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'home/index'
   resources :students
   resources :posts
   get "/forum", to: "posts#index"
-  get "/student", to: "students#index"
+  get "/register", to: "students#new"
+  get "/students", to: "student#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -11,4 +13,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root "home#index"
+
 end
